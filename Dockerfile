47c38d1c0ab730f16376306dc6d621ec9107b84d
Max Torf <torfstack@gmail.com>
2024-02-03 17:05:47 +0100

    configured helm

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM alpine:3.19.1
 
 RUN mkdir /opt/sciffr
 
-COPY sciffr /opt/sciffr/sciffr
+COPY bin/sciffr /opt/sciffr/sciffr
 
 CMD ["./opt/sciffr/sciffr"]
 
--- backend/backend.go
+++ backend/backend.go
@@ -9,6 +9,10 @@ func New() *Backend {
 	return &Backend{}
 }
 
-func (b *Backend) Register(engine *gin.Engine) {
+func (b *Backend) Register(e *gin.Engine) {
+	e.GET("/encrypt", encrypt)
+}
 
+func encrypt(c *gin.Context) {
+	c.JSON(200, "ENCRYPTED")
 }
